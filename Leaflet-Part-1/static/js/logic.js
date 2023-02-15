61c1b08118e3cc8baff7ca4f3a22d0ba029d0387
Danielle Anderson <dganderson94@gmail.com>
2023-02-15 14:42:35 -0600

    earthquake map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,24 +1,41 @@
 // API endpoint
-var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
 // GET request
+d3.json(queryURL).then(function (data) {
+    createFeatures(data.features);
+  });
 
 //// createFeatures ////
+function createFeatures(earthquakeData) {
+
+    // popup functionality
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+  
+    // geoJSON
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+  
+    createMap(earthquakes);
+
+}
 
 //// createMap ////
+function createMap(earthquakes) {
 
-// Tile layer
-var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+    // Tile layer
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-// baseMaps
+    // Map object
+    L.map("map", {
+    center: [0, 0],
+    zoom: 2,
+    layers: [street, earthquakes]
+    });
 
-// overlay
-
-// Map object
-var myMap = L.map("map", {
-  center: [40, -100],
-  zoom: 5,
-  layers: [street]
-});
+}
