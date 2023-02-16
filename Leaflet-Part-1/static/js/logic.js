b30f6d561cba6aed5a6c1e1c0f2179eb55ae7bcc
Danielle Anderson <dganderson94@gmail.com>
2023-02-15 18:41:52 -0600

    wrapped up, readme

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -9,16 +9,27 @@ d3.json(queryURL).then(function (data) {
 //// createFeatures ////
 function createFeatures(earthquakeData) {
 
+    var grade = [170, 130, 90, 50, 10];
+    var colors = ["#253494","#2c7fb8","#41b6c4","#7fcdbb","#c7e9b4","#ffffcc"]
+
     // popup functionality
     function onEachFeature(feature, layer) {
         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+
+    // markerColor
+    function markerColor(depth) {
+        for(let i = 0; i < 5; i++) {
+            if (depth > grade[i]) return  colors[i];
+        }
+        return colors[6];
     }
 
     // markers
     function pointToLayer(feature, loc) {
         var marker = {
             radius: feature.properties.mag ** 2 / 4,
-            fillColor: "#ff7800",
+            fillColor: markerColor(feature.geometry.coordinates[2]),
             color: "#000",
             weight: 1,
             opacity: 1,
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leaflet-challenge
+
+This repository contains HTML, CSS, and Javascript that shows earthquake data on a world map, using Leaflet.
